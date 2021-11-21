160ea41ae44a49698cbd70be9d1649d6406fcd24
DomenicoParente <domenico.parente@mail.polimi.it>
2021-11-21 13:06:35 -0600

    starter project

---
+++ Assets/SnackScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnackScript : MonoBehaviour
+{
+    public GameObject Snack;
+
+    public void Add() {
+        Instantiate(Snack, transform.position, transform.rotation);
+    }
+       
+}
